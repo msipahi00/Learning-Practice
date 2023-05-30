4cc3178de54a34bfcc2c4c807c826159cbaefafe
msipahi00 <mark.sipahi@gmail.com>
2023-05-30 11:57:58 -0500

    working on simon game and learning jquery

---
+++ UdemyCompleteWebDevCourse/DOMModel/jQueryProject/index.js
@@ -0,0 +1,6 @@
+$("button").on("click", function() { 
+    $("h1").slideUp().slideDown().animate({opacity:0.5, fontSize:50})
+});
+
+/*slideUp, slideDown, fadeOut, fadeIn, fadeToggle, slideToggle, hide, show, toggle*/
+
